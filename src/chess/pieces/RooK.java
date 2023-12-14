4c062556d9c141b55de0f1d430c012be7ac44ec8
Pedro Luis Somera <pls11@dspcontroles.com.br>
2023-12-13 21:27:03 -0300

    Placing pieces on the board

---
+++ src/chess/pieces/RooK.java
@@ -0,0 +1,27 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class RooK extends ChessPiece{
+
+	public RooK(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString(){
+		return "R";
+	}
+	
+	
+}
+
+
+	
+
+
+
+
+
